606ed92e8bc9a6738996f9911ce6b80e2e75f546
oliprivate <oliverlouis@me.com>
2024-05-26 23:12:17 +0200

    Change styles to scss. Create search-input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SearchInputComponent} from "./search-input/search-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dictionary-web-app';
