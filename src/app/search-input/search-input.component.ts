4128d03969cf35a197b832f26b86600c600f9aa5
oliprivate <oliverlouis@me.com>
2024-05-28 07:40:39 +0200

    model search input value via ngModel

--- src/app/search-input/search-input.component.ts
+++ src/app/search-input/search-input.component.ts
@@ -1,21 +1,27 @@
 import {Component, Input} from '@angular/core';
 import {SearchService} from "../services/search.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'dwa-search-input',
   standalone: true,
   templateUrl: './search-input.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrl: './search-input.component.scss'
 })
 export class SearchInputComponent {
   @Input()
   public placeholder?: string;
 
+  public inputValue = '';
+
   constructor(private searchService: SearchService) {
   }
 
   public onSearch() {
-    this.searchService.search('grotesque');
+    this.searchService.search(this.inputValue);
   }
 }
 
